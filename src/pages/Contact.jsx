35c6ccae99ea996ccd105fdbcc9e01c91a532210
Nishika Singh <nishikasingh0307@gmail.com>
2026-08-04 12:45:23 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,15 +2,29 @@ const Contact = () => {
   return (
     <section id="contact">
       <h2>CONTACT</h2>
-    
-      <div className="card">
-        <h3>
-        I’m open to opportunities, internships, and collaborations.  
-        Feel free to reach out!
-        </h3>
-        <p>📧 Email: <a href="mailto:nishikasingh0307@gmail.com">nishikasingh0307@gmail.com </a></p>
-        <p>💼 LinkedIn: <a href="https://www.linkedin.com/in/nishika-singh-as2025/">linkedin.com/in/nishika-singh-as2025/</a></p>
-        <p>💻 GitHub: <a href="https://github.com/nishikasingh31">github.com/nishikasingh31</a></p>
+      <div className="card" style={{ "--accent": "#34d399" }}>
+        <p>
+          I'm open to opportunities, internships, and collaborations. Feel
+          free to reach out!
+        </p>
+        <p>
+          📧 Email:{" "}
+          <a href="mailto:nishikasingh0307@gmail.com">
+            nishikasingh0307@gmail.com
+          </a>
+        </p>
+        <p>
+          💼 LinkedIn:{" "}
+          <a href="https://www.linkedin.com/in/nishika-singh-as2025/">
+            linkedin.com/in/nishika-singh-as2025/
+          </a>
+        </p>
+        <p>
+          💻 GitHub:{" "}
+          <a href="https://github.com/nishikasingh31">
+            github.com/nishikasingh31
+          </a>
+        </p>
       </div>
     </section>
   );
