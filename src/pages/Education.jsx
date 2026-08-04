5e219a18dba45fe3e2c95a3bc3f3bad347a37d83
Nishika Singh <nishikasingh0307@gmail.com>
2026-08-04 12:59:59 +0530

    Update Education.jsx

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -2,14 +2,14 @@ const Education = () => {
   return (
     <section id="education">
       <h2>EDUCATION</h2>
-      <div className="card" style={{ "--accent": "#38bdf8" }}>
+      <div className="card" style={{ "--accent": "#facc15" }}>
         <h3 className="accent-title">
           <strong>🎓 Bachelor of Technology – Computer Science Engineering (AI & ML)</strong>
         </h3>
         <p>📍 Chhattisgarh Swami Vivekanand Technical University, Bhilai, Chhattisgarh</p>
         <p>📌 2021 – 2025 | CGPA: 7.3</p>
       </div>
-      <div className="card" style={{ "--accent": "#facc15" }}>
+      <div className="card" style={{ "--accent": "#a78bfa" }}>
         <h3 className="accent-title">
           <strong>🎓 Senior Secondary (Class XII)</strong>
         </h3>
